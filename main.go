0a9ddaf8176d8b2972e4c7619f94011c6921deac
Maicon Siqueira <maicon.siqueira@live.com>
2022-02-27 11:46:14 -0300

    first commit

--- main.go
+++ main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func main() {
-	cmd := exec.Command("git", "remote", "--verbose", "|", "awk '{print $2}'", "|", "head", "-n", "1")
 
-	stdout, err := cmd.Output()
+	cmd := "git tag --sort=taggerdate | tail -1"
+	tag := exec.Command("bash", "-c", cmd)
+	stdout, err := tag.Output()
+	tagParse := string(stdout)
+	tagParse = strings.TrimSuffix(tagParse, "\n")
 
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		log.Fatal(err)
+	}
+
+	semverProd, _ := regexp.MatchString("^([0-9]+.[0-9.].[0-9])$", tagParse)
+	semverDev, _ := regexp.MatchString("^([0-9]+.[0-9.]+.[0-9]-rc.[0-9])$", tagParse)
+
+	if semverProd == true {
+		fmt.Printf("Deu bom: é prod: %s \n", tagParse)
+	} else if semverDev == true {
+		fmt.Printf("Deu bom: é dev: %s \n", tagParse)
 	} else {
-		fmt.Print(string(stdout))
+		fmt.Printf("nao é nenhum, oq veio foi esse: %s \n", tagParse)
 	}
 }
